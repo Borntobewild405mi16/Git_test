cbd4b277a8362401ba38d4d0aa22371870e0d4c9
Stefan <stefan.de.vries@cre8it.nl>
2014-03-14 15:15:42 +0100

    Added fourth

--- index.php
+++ index.php
@@ -3,3 +3,4 @@ echo "Hello world\n"; //master
 echo "Hello new feature\n"; //new_feature
 echo "A second change\n"; //new_feature
 echo "A third change\n"; //new_feature
+echo "A fourth change\n"; //feature_two
